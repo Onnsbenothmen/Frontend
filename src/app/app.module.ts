a8596880af9af09d4e74884429513339a1e4a5a5
Ons <Ons@gmail.com>
2023-11-25 22:15:13 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DockerComponent } from './docker/docker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DockerComponent
   ],
   imports: [
     BrowserModule,
